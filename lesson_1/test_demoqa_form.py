c8df4db2c262e184640b69f31d601b58329f251a
Ikonnikov <Ikonnikov.Oleg47@gmail.com>
2023-07-04 22:16:56 +0300

    lesson 5 added checks + README

--- lesson_1/test_demoqa_form.py
+++ lesson_1/test_demoqa_form.py
@@ -1,13 +1,13 @@
 from selene.core import command
-from selene import browser, have
+from selene import browser, have, by
 
 browser.config.window_width = 2000
 browser.config.window_height = 1700
 def test_demoqa_form():
     browser.open('https://demoqa.com/automation-practice-form')
     browser.all('footer').perform(command.js.remove)
-    browser.element("[id='firstName']").send_keys("Soska")
-    browser.element("[id='lastName']").send_keys('Nerealka')
+    browser.element("[id='firstName']").send_keys("Olga")
+    browser.element("[id='lastName']").send_keys('Buzova')
     browser.element('[id="userEmail"]').send_keys('onlyfans@gmail.com')
     browser.element('[for="gender-radio-3"]').click()
     browser.element("[id='userNumber']").send_keys("1231233423")
@@ -23,7 +23,8 @@ def test_demoqa_form():
     browser.element('[id="currentAddress"]').type('chikago john')
     browser.element('[for="hobbies-checkbox-3"]').click()
     browser.element('[id="react-select-3-option-1"]').click
-    # browser.element('[id="city"]').click()
-    # browser.element('[id="react-select-4-option-1"]').click()
-    # browser.element('[id="submit"]').click()
-    # browser.element('[id="closeLargeModal"]').click()
+    browser.element('#state').click()
+    browser.all("#state div").element_by(have.exact_text("Uttar Pradesh")).click()
+    browser.element('#city').click()
+    browser.all('#city div').element_by(have.exact_text("Lucknow")).click()
+    browser.element('#submit').perform(command.js.click)
---
+++ lesson_5/demoqa_test/test_demoqa.py
@@ -0,0 +1,40 @@
+import os
+from selene import browser, command
+import match
+
+def test_cpmplete_todo():
+    browser.open('/')
+    browser.element('#firstName').type('Viktor')
+    browser.element('#lastName').type('Coy')
+    browser.element('#userEmail').type('Coy@gmail.com')
+    browser.element('.custom-control-label[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type(2340823094)
+    browser.element('#dateOfBirthInput').click()
+    browser.element(".react-datepicker__year-select [value='1988']").click()
+    browser.element(".react-datepicker__month-select [value='3']").click()
+    browser.element('.react-datepicker__day--013').click()
+    browser.element('#subjectsInput').type('Arts').press_enter()
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('label[for="hobbies-checkbox-2"]').click()
+    browser.element('label[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('img/WoWScrnShot_012423_172906.jpg'))
+    browser.element('#currentAddress').type('Moscow newer sleep')
+    browser.element('#state').click()
+    browser.all("#state div").element_by(match.exact_text("Uttar Pradesh")).click()
+    browser.element('#city').click()
+    browser.all('#city div').element_by(match.exact_text("Lucknow")).click()
+    browser.element('#submit').perform(command.js.click)
+
+
+    browser.element('.modal-header').should(match.exact_text('Thanks for submitting the form'))
+    browser.all('.modal-body tr td')[1].should(match.exact_text('Viktor Coy'))
+    browser.all('.modal-body tr td')[3].should(match.exact_text('Coy@gmail.com'))
+    browser.all('.modal-body tr td')[5].should(match.exact_text('Male'))
+    browser.all('.modal-body tr td')[7].should(match.exact_text('2340823094'))
+    browser.all('.modal-body tr td')[9].should(match.exact_text('13 April,1988'))
+    browser.all('.modal-body tr td')[11].should(match.exact_text('Arts'))
+    browser.all('.modal-body tr td')[13].should(match.exact_text('Sports, Reading, Music'))
+    browser.all('.modal-body tr td')[15].should(match.exact_text('WoWScrnShot_012423_172906.jpg'))
+    browser.all('.modal-body tr td')[17].should(match.exact_text('Moscow newer sleep'))
+    browser.all('.modal-body tr td')[19].should(match.exact_text('Uttar Pradesh Lucknow'))
+    browser.element('#closeLargeModal').click()
