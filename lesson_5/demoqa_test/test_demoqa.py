6005fc4d91f987374fa53ef63ac7517b6cfa66e9
Ikonnikov <Ikonnikov.Oleg47@gmail.com>
2023-07-06 23:13:38 +0300

    lesson 6 new check

--- lesson_5/demoqa_test/test_demoqa.py
+++ lesson_5/demoqa_test/test_demoqa.py
@@ -26,15 +26,30 @@ def test_cpmplete_todo():
     browser.element('#submit').perform(command.js.click)
 
 
-    browser.element('.modal-header').should(match.exact_text('Thanks for submitting the form'))
-    browser.all('.modal-body tr td')[1].should(match.exact_text('Viktor Coy'))
-    browser.all('.modal-body tr td')[3].should(match.exact_text('Coy@gmail.com'))
-    browser.all('.modal-body tr td')[5].should(match.exact_text('Male'))
-    browser.all('.modal-body tr td')[7].should(match.exact_text('2340823094'))
-    browser.all('.modal-body tr td')[9].should(match.exact_text('13 April,1988'))
-    browser.all('.modal-body tr td')[11].should(match.exact_text('Arts'))
-    browser.all('.modal-body tr td')[13].should(match.exact_text('Sports, Reading, Music'))
-    browser.all('.modal-body tr td')[15].should(match.exact_text('WoWScrnShot_012423_172906.jpg'))
-    browser.all('.modal-body tr td')[17].should(match.exact_text('Moscow newer sleep'))
-    browser.all('.modal-body tr td')[19].should(match.exact_text('Uttar Pradesh Lucknow'))
-    browser.element('#closeLargeModal').click()
+    # browser.element('.modal-header').should(match.exact_text('Thanks for submitting the form'))
+    # browser.all('.modal-body tr td')[1].should(match.exact_text('Viktor Coy'))
+    # browser.all('.modal-body tr td')[3].should(match.exact_text('Coy@gmail.com'))
+    # browser.all('.modal-body tr td')[5].should(match.exact_text('Male'))
+    # browser.all('.modal-body tr td')[7].should(match.exact_text('2340823094'))
+    # browser.all('.modal-body tr td')[9].should(match.exact_text('13 April,1988'))
+    # browser.all('.modal-body tr td')[11].should(match.exact_text('Arts'))
+    # browser.all('.modal-body tr td')[13].should(match.exact_text('Sports, Reading, Music'))
+    # browser.all('.modal-body tr td')[15].should(match.exact_text('WoWScrnShot_012423_172906.jpg'))
+    # browser.all('.modal-body tr td')[17].should(match.exact_text('Moscow newer sleep'))
+    # browser.all('.modal-body tr td')[19].should(match.exact_text('Uttar Pradesh Lucknow'))
+    # browser.element('#closeLargeModal').click()
+
+    browser.element('.table').all('td').even.should(
+        match.exact_texts(
+            'Viktor Coy',
+            'Coy@gmail.com',
+            'Male',
+            '2340823094',
+            '13 April,1988',
+            'Arts',
+            'Sports, Reading, Music',
+            'WoWScrnShot_012423_172906.jpg',
+            'Moscow newer sleep',
+            'Uttar Pradesh Lucknow',
+        )
+    )
